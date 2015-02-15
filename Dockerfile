ed056b443259ed8d5c75bc315ef29ecb2f414e68
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-02-15 18:35:00 +0000

    Strip out useless registry requests

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,6 @@ run curl -s https://go.googlecode.com/files/go1.1.1.linux-amd64.tar.gz | tar -v 
 env PATH  /usr/local/go/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 env GOPATH  /go
 env CGO_ENABLED 0
-run cd /tmp && echo 'package main' > t.go && go test -a -i -v
 
 add . /docker-registry.git/contrib/golang_impl
 run cd /docker-registry.git/contrib/golang_impl && make && cp bin/docker-registry /usr/local/bin/
--- handler.go
+++ handler.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
-	"time"
 )
 
 type Mapping struct {
 	Method  string
 	Regexp  *regexp.Regexp
 	Handler func(http.ResponseWriter, *http.Request, [][]string)
-}
-
-func GenerateUUID() string {
-	f, _ := os.Open("/dev/urandom")
-	b := make([]byte, 16)
-	f.Read(b)
-	f.Close()
-	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
 type Handler struct {
@@ -42,57 +32,6 @@ func (h *Handler) GetPing(w http.ResponseWriter, r *http.Request, p [][]string) 
 	w.Header().Add("X-Docker-Registry-Version", "0.0.1")
 	w.WriteHeader(200)
 	fmt.Fprint(w, "pong")
-}
-
-func (h *Handler) GetUsers(w http.ResponseWriter, r *http.Request, p [][]string) {
-	w.WriteHeader(201)
-	fmt.Fprint(w, "OK")
-}
-
-func (h *Handler) GetRepositoryImages(w http.ResponseWriter, r *http.Request, p [][]string) {
-	repo := &Repository{h.DataDir + "/repositories/" + p[0][2]}
-	if images, err := repo.Images(); err == nil {
-		h.WriteJsonHeader(w)
-		h.WriteEndpointsHeader(w, r)
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, string(images))
-	} else {
-		logger.Error(err.Error())
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	return
-}
-
-func (h *Handler) GetImageAncestry(w http.ResponseWriter, r *http.Request, p [][]string) {
-	idPrefix := p[0][2]
-	if paths, err := filepath.Glob(h.DataDir + "/images/" + idPrefix + "*"); err == nil {
-		if len(paths) > 0 {
-			image := &Image{paths[0]}
-			if out, err := json.Marshal(image.Ancestry()); err == nil {
-				h.WriteJsonHeader(w)
-				w.WriteHeader(http.StatusOK)
-				fmt.Fprintf(w, string(out))
-				return
-			}
-		}
-	}
-	http.NotFound(w, r)
-}
-
-func (h *Handler) GetImageLayer(w http.ResponseWriter, r *http.Request, p [][]string) {
-	idPrefix := p[0][2]
-	if paths, err := filepath.Glob(h.DataDir + "/images/" + idPrefix + "*"); err == nil {
-		image := &Image{paths[0]}
-		file, err := os.Open(image.LayerPath())
-		if err == nil {
-			w.Header().Add("Content-Type", "application/x-xz")
-			w.WriteHeader(http.StatusOK)
-			io.Copy(w, file)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-		}
-	}
 }
 
 func (h *Handler) GetImageJson(w http.ResponseWriter, r *http.Request, p [][]string) {
@@ -116,21 +55,6 @@ func (h *Handler) GetImageJson(w http.ResponseWriter, r *http.Request, p [][]str
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func (h *Handler) GetRepositoryTags(w http.ResponseWriter, r *http.Request, p [][]string) {
-	repo := &Repository{h.DataDir + "/repositories/" + p[0][2]}
-	tagsJson, err := json.Marshal(repo.Tags())
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, err.Error())
-		return
-	}
-	h.WriteJsonHeader(w)
-	h.WriteEndpointsHeader(w, r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, string(tagsJson))
-	return
-}
-
 func (h *Handler) PutImageResource(w http.ResponseWriter, r *http.Request, p [][]string) {
 	imageId := p[0][2]
 	tagName := p[0][3]
@@ -140,28 +64,6 @@ func (h *Handler) PutImageResource(w http.ResponseWriter, r *http.Request, p [][
 		logger.Error(err.Error())
 	} else {
 		w.WriteHeader(http.StatusOK)
-	}
-}
-
-func (h *Handler) PutRepositoryTags(w http.ResponseWriter, r *http.Request, p [][]string) {
-	repoName := p[0][2]
-	path := h.DataDir + "/repositories/" + repoName + "/tags/" + p[0][3]
-	err := writeFile(path, r.Body)
-	if err != nil {
-		logger.Error(err.Error())
-	} else {
-		w.WriteHeader(http.StatusOK)
-	}
-}
-
-func (h *Handler) PutRepositoryImages(w http.ResponseWriter, r *http.Request, p [][]string) {
-	repoName := p[0][2]
-	repo := &Repository{h.DataDir + "/repositories/" + repoName}
-	err := writeFile(repo.ImagesPath(), r.Body)
-	if err != nil {
-		logger.Error(err.Error())
-	} else {
-		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
@@ -200,35 +102,40 @@ func (h *Handler) doHandle(w http.ResponseWriter, r *http.Request) (ok bool) {
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	started := time.Now()
-	uuid := GenerateUUID()
-	w.Header().Add("X-Request-ID", uuid)
-	logger.Info(fmt.Sprintf("%s got request %s %s", uuid, r.Method, r.URL.String()))
+	logger.Info(fmt.Sprintf("Got request %s %s", r.Method, r.URL.String()))
 	if ok := h.doHandle(w, r); !ok {
 		logger.Info("returning 404")
 		http.NotFound(w, r)
 	}
-	logger.Info(fmt.Sprintf("%s finished request in %.06f", uuid, time.Now().Sub(started).Seconds()))
+}
+
+func DummyResponse(status int) (func(http.ResponseWriter, *http.Request, [][]string)) {
+	return func (w http.ResponseWriter, r *http.Request, p [][]string) {
+		logger.Info(fmt.Sprintf("ignoring request, returning %d", status))
+		w.WriteHeader(status)
+	}
 }
 
 func NewHandler(dataDir string) (handler *Handler) {
 	handler = &Handler{DataDir: dataDir}
 
+	// this isn't a full registry
+	handler.Map("GET", "users", DummyResponse(http.StatusNotImplemented))
+	handler.Map("GET", "images/(.*?)/ancestry", DummyResponse(http.StatusNotImplemented))
+	handler.Map("GET", "images/(.*?)/layer", DummyResponse(http.StatusNotImplemented))
+	handler.Map("GET", "repositories/(.*?)/tags", DummyResponse(http.StatusNotImplemented))
+	handler.Map("GET", "repositories/(.*?)/images", DummyResponse(http.StatusNotImplemented))
+	handler.Map("PUT", "repositories/(.*?)/tags/(.*)", DummyResponse(http.StatusOK))
+	handler.Map("PUT", "repositories/(.*?)/images", DummyResponse(http.StatusNoContent))
+
 	// dummies
 	handler.Map("GET", "_ping", handler.GetPing)
-	handler.Map("GET", "users", handler.GetUsers)
 
 	// images
-	handler.Map("GET", "images/(.*?)/ancestry", handler.GetImageAncestry)
-	handler.Map("GET", "images/(.*?)/layer", handler.GetImageLayer)
 	handler.Map("GET", "images/(.*?)/json", handler.GetImageJson)
 	handler.Map("PUT", "images/(.*?)/(.*)", handler.PutImageResource)
 
 	// repositories
-	handler.Map("GET", "repositories/(.*?)/tags", handler.GetRepositoryTags)
-	handler.Map("GET", "repositories/(.*?)/images", handler.GetRepositoryImages)
-	handler.Map("PUT", "repositories/(.*?)/tags/(.*)", handler.PutRepositoryTags)
-	handler.Map("PUT", "repositories/(.*?)/images", handler.PutRepositoryImages)
 	handler.Map("PUT", "repositories/(.*?)/$", handler.PutRepository)
 	return
 }
