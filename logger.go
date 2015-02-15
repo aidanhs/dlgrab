7c3883480412e3d766ca9e09c29b94bc8aa75a6c
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-02-15 21:29:50 +0000

    Log to stderr

--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type Logger struct {
@@ -18,20 +19,20 @@ const (
 func (l *Logger) Error(s ...interface{}) {
 	if l.Level <= ERROR {
 		str := fmt.Sprint(s...)
-		fmt.Println("ERROR", str)
+		fmt.Fprintln(os.Stderr, "ERROR", str)
 	}
 }
 
 func (l *Logger) Debug(s ...interface{}) {
 	if l.Level <= DEBUG {
 		str := fmt.Sprint(s...)
-		fmt.Println("DEBUG", str)
+		fmt.Fprintln(os.Stderr, "DEBUG", str)
 	}
 }
 
 func (l *Logger) Info(s ...interface{}) {
 	if l.Level <= INFO {
 		str := fmt.Sprint(s...)
-		fmt.Println("INFO", str)
+		fmt.Fprintln(os.Stderr, "INFO", str)
 	}
 }
